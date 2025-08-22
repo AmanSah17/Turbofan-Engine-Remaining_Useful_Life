1fd2ea56fbc929cea723c559742d7402effa29b2
Aman Sah <amansah1717@gmail.com>
2025-08-22 23:45:23 +0530

    target_metric_eval

--- utils.py
+++ utils.py
@@ -1,20 +1,6 @@
 import os
-from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint
-
-def get_callbacks(model_name):
-    tensorboard_cb = TensorBoard(log_dir=f"logs/{model_name}")
-    checkpoint_cb = ModelCheckpoint(f"saved_models/{model_name}.h5", save_best_only=True)
-    return [tensorboard_cb, checkpoint_cb]
-
-
-
-
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-import os
-
+import tensorflow as tf
+from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint , EarlyStopping
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
@@ -22,9 +8,8 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Dropout
 from tensorflow.keras.optimizers import Adam
 import tensorflow.keras.backend as K
-import tensorflow as tf
 import matplotlib.pyplot as plt
-import os
+
 
 
 
@@ -34,12 +19,15 @@ SENSOR_COLS = [f"sensor{i}" for i in range(1, 22)]  # sensors 1 to 21
 ALL_COLUMNS = ["unit", "time_cycles"] + OP_COLS + SENSOR_COLS
 
 
-SEQUENCE_LENGTH = 30
+SEQUENCE_LENGTH = 25
 
 
 
 
-
+def get_callbacks(model_name):
+    tensorboard_cb = TensorBoard(log_dir=f"logs/{model_name}")
+    checkpoint_cb = ModelCheckpoint(f"saved_models/{model_name}.h5", save_best_only=True)
+    return [tensorboard_cb, checkpoint_cb]
 
 
 
@@ -105,3 +93,70 @@ def get_callbacks(model_name):
         EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True),
         ModelCheckpoint(f"models/{model_name}.h5", save_best_only=True)
     ]
+
+
+
+
+OP_SETTING_COLUMNS = ['op_setting_{}'.format(x) for x in range(1, 4)]
+SENSOR_COLUMNS = ['sensor_{}'.format(x) for x in range(1, 22)]
+
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+DATA_DIR = os.path.join(CURRENT_DIR, 'CMaps')
+
+
+def read_data(filepath):
+    '''
+    Reads `filepath` as space separated file and returns pd.DataFrame
+    '''
+    col_names = ['unit', 'time_cycles'] + OP_SETTING_COLUMNS + SENSOR_COLUMNS
+    return pd.read_csv(
+        filepath,
+        sep='\s+',
+        header=None,
+        names=col_names
+    )
+
+def read_dataset(dataset_name):
+    '''
+    Reads TRAIN, TEST and RUL datasets for specified dataset name
+
+    Parameters
+    ----------
+    dataset_name : str, name of the dataset, e.g. 'FD001'
+
+    Returns
+    -------
+    a tuple of (pd.DataFrame, pd.DataFrame, np.array) for TRAIN, TEST AND RUL
+    datasets correspondingly
+    '''
+    TRAIN_FILE = os.path.join(DATA_DIR, f'train_{dataset_name}.txt')
+    TEST_FILE = os.path.join(DATA_DIR, f'test_{dataset_name}.txt')
+    TEST_RUL_FILE = os.path.join(DATA_DIR, f'RUL_{dataset_name}.txt')
+
+    train_data = read_data(TRAIN_FILE)
+    test_data = read_data(TEST_FILE)
+    test_rul = np.loadtxt(TEST_RUL_FILE)
+
+    return train_data, test_data, test_rul
+
+
+def calculate_RUL(X, upper_threshold=None):
+    '''
+    Calculate Remaining Useful Life per `unit`
+
+    Parameters
+    ----------
+    X : pd.DataFrame, with `unit` and `time_cycles` columns
+    upper_threshold: int, limit maximum RUL valus, default is None
+
+    Returns
+    -------
+    np.array with Remaining Useful Life values
+    '''
+    lifetime = X.groupby(['unit'])['time_cycles'].transform(max)
+    rul = lifetime - X['time_cycles']
+
+    if upper_threshold:
+        rul = np.where(rul > upper_threshold, upper_threshold, rul)
+
+    return rul
